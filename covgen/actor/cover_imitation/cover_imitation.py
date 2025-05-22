fc1a4f4f8806f9333168ba1878049b6eece6e1ae
吴子豪 <vortez.wohl@gmail.com>
2025-05-22 19:52:09 +0800

    fix: 修复无法生成书名的bug

--- covgen/actor/cover_imitation/cover_imitation.py
+++ covgen/actor/cover_imitation/cover_imitation.py
@@ -8,15 +8,21 @@ class CoverImitation(object):
     def __init__(self, title: str, base64_image: str, image_format: str):
         self._base64_image = base64_image
         self._image_format = image_format
-        self._prompt = ImageUnderstanding(base64_image=base64_image, image_format=image_format).image_features().get('output')
+        self._prompt: dict = dict({'图片风格为': '', '图片用途': ''})
+        self._prompt.update(ImageUnderstanding(
+            base64_image=base64_image,
+            image_format=image_format
+        ).image_features().get('output'))
         if self._prompt is not None:
-            del self._prompt['文案内容']['作者']
+            self._prompt['图片风格为'] = self._prompt['呈现类型']
+            del self._prompt['呈现类型']
             self._prompt['文案内容']['书名'] = title
+        self._prompt = f'请参考图片描述: {json.dumps(self._prompt, ensure_ascii=False)}. 生成一份带有书名的书封图, 书名为《{title}》'
 
-    def generate(self, size: str = '720x960') -> list[str]:
+    def generate(self, size: str = '720x960') -> str:
         resp = ark_client.images.generate(
             model=ark_image_model,
-            prompt=json.dumps(self._prompt, ensure_ascii=False),
+            prompt=self._prompt,
             size=size,
             response_format='url',
             watermark=False
@@ -26,4 +32,4 @@ class CoverImitation(object):
 
 if __name__ == '__main__':
     from covgen.util.image_stringifier import stringify
-    print(CoverImitation('我是吴子豪', stringify(r'D:\project\covgen\resource\knowledge_base\female_oriented\ancient_romance\eastasia_anime\3.png'), 'png').generate())
+    print(CoverImitation('六百六十六', stringify(r'D:\project\covgen\resource\knowledge_base\female_oriented\ancient_romance\eastasia_anime\2.png'), 'png').generate())
--- covgen/actor/image_understanding/image_understanding.py
+++ covgen/actor/image_understanding/image_understanding.py
@@ -13,47 +13,22 @@ class ImageUnderstanding(object):
     def image_features(self, max_retries: int = 5) -> dict | None:
         _retries = 0
         outer_keys = ['chain_of_thought', 'output']
-        keys = ['呈现类型', '人物形象',
-                '人物外貌', '人物年龄',
-                '人物神态(眼神、动作、表情、心情)', '人物种族/地域特征',
-                '人物的其他补充信息', '图像氛围',
-                '图像情绪', '图像构图特点',
-                '色彩特点', '风格特点',
-                '文案内容', '文案布局描述',
-                '字体风格描述']
-        sub_keys_0 = ['主体构图', '背景构图', '文案构图']
-        sub_keys_1 = ['作者', '书名', '其他修饰性文字描述', '其他功能性文字描述']
         while True:
             if _retries >= max_retries:
                 return None
             try:
                 resp = ark_client.chat.completions.create(
                     model=ark_language_model,
-                    messages=self._prompt.message_with_image
+                    messages=self._prompt.message_with_image,
+                    temperature=0.1,
+                    top_p=0.1
                 ).choices[0].message.content
                 resp_dict = json.loads(resp)
                 for o_k in outer_keys:
                     if o_k not in resp_dict.keys():
                         continue
-                output_dict = resp_dict[outer_keys[1]]
-                for k in keys:
-                    if k not in output_dict.keys():
-                        continue
-                if '图像构图特点' in output_dict.keys():
-                    if isinstance(output_dict['图像构图特点'], dict):
-                        for s_k in sub_keys_0:
-                            if s_k not in output_dict['图像构图特点'].keys():
-                                continue
-                    else:
-                        continue
-                if '文案内容' in output_dict.keys():
-                    if isinstance(output_dict['文案内容'], dict):
-                        for s_k in sub_keys_1:
-                            if s_k not in output_dict['文案内容'].keys():
-                                continue
-                    else:
-                        continue
-                return resp_dict
+                output_dict = resp_dict['output']
+                return output_dict
             except json.decoder.JSONDecodeError:
                 continue
             finally:
