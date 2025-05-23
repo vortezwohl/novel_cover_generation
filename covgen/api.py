a5dc5ffe8638f09ed00d75b6a7d8436c4ba34022
吴子豪 <vortez.wohl@gmail.com>
2025-05-23 22:11:04 +0800

    feat: 实现随机字体生成

--- covgen/api.py
+++ covgen/api.py
@@ -1,8 +1,10 @@
+import logging
 import os.path
 from concurrent.futures import ThreadPoolExecutor
 from random import choice
 
 from resource import FEMALE_ORIENTED_KB_ROOT, EN_FONT_ROOT, ZH_FONT_ROOT
+from covgen.enum.lang import Lang
 from covgen.util.image_stringifier import image_to_base64
 from covgen.util.read_file import get_files
 from covgen.data import CoverGenReq, CoverGenResp
@@ -11,14 +13,29 @@ from covgen.enum.subject import Subject
 from covgen.actor.cover_imitation.cover_imitation import CoverImitation
 from covgen.actor.title_generation.simple_title_generation import SimpleTitleGeneration
 from covgen.actor.novel_theme_recognition.novel_theme_recognition import NovelThemeRecognition
+from covgen.actor.novel_language_recognition.novel_language_recognition import NovelLanguageRecognition
+
+log = logging.getLogger('covgen')
 
 
 def simple_cover_gen(req: CoverGenReq) -> dict:
+    def choose_title_font():
+        font_root = None
+        lang = NovelLanguageRecognition(title=req.title, novel=req.novel).recognize()
+        match lang:
+            case Lang.Zh:
+                font_root = ZH_FONT_ROOT
+            case Lang.En:
+                font_root = EN_FONT_ROOT
+        font = choice(get_files(font_root))
+        log.debug(f'font: {font}')
+        return font
+
     def from_b64_image_to_cover(b64_image: str) -> str:
         nonlocal req
         return SimpleTitleGeneration(
             title=req.title, base64_image=CoverImitation(base64_image=b64_image, image_format='png').generate(),
-            title_color=req.title_color, title_font=req.title_font,
+            title_color=req.title_color, title_font=choose_title_font(),
             title_height_correction=req.title_height_correction
         ).generate()
 
@@ -58,8 +75,6 @@ if __name__ == '__main__':
     images = simple_cover_gen(CoverGenReq(
         novel='The Secret I Heard',
         title='The Secret I Heard',
-        title_color=(255, 255, 255),
-        title_font=r'D:\project\covgen\resource\font\Aston Script.ttf',
         title_height_correction=-384))['b64_images']
     for i, img in enumerate(images):
         img.save(rf'D:\project\covgen\test\output\cover_imitation_simple_{i}.png')
