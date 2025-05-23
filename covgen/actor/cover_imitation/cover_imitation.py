0e96312e6675a6426f054ab7b7868577ce185b7b
吴子豪 <vortez.wohl@gmail.com>
2025-05-23 16:07:52 +0800

    feat: 实现单字体书名生成

--- covgen/actor/cover_imitation/cover_imitation.py
+++ covgen/actor/cover_imitation/cover_imitation.py
@@ -16,7 +16,7 @@ log = logging.getLogger('covgen')
 class CoverImitation(object):
     def __init__(self, title: str, base64_image: str,
                  image_format: str, title_color: tuple | str,
-                 title_font: str = 'arial.ttf', title_height_correction: float = 0):
+                 title_font: str, title_height_correction: float = 0.):
         self._title = title
         self._title_font = title_font
         self._title_height_correction = - title_height_correction
@@ -30,7 +30,7 @@ class CoverImitation(object):
         log.debug(f'cover_imitation_prompt: {self._prompt}')
 
     def generate(self, size: tuple = (720, 960)) -> Image:
-        text_size = 3 * size[0] // 4, size[1] // 10
+        text_size = 6 * size[0] // 7, size[1] // 2
         image_center_coordinate = size[0] / 2, size[1] / 2
         text_box_coordinate = (math.floor(image_center_coordinate[0] - text_size[0] / 2),
                                math.floor(self._title_height_correction +
@@ -40,27 +40,33 @@ class CoverImitation(object):
         text_draw = ImageDraw.Draw(text_image)
         font_size = min_font_size = 0
         max_font_size = size[0]
-        factor = 100
+        factor = 10
         for fz in range(max_font_size * factor, min_font_size * factor - 1, -1):
             _fz = fz / factor
             font = ImageFont.truetype(font=self._title_font, size=_fz)
-            _text_width = text_draw.textlength(text=self._title, font=font)
+            max_len_line = self._title
+            if '\n' in self._title:
+                max_len_line = sorted(self._title.splitlines(keepends=False), key=lambda x: len(x), reverse=True)[0]
+            _text_width = text_draw.textlength(text=max_len_line, font=font)
             if _text_width <= text_size[0]:
                 font_size = _fz
                 break
         print('font size', font_size)
+        x, y = 0, 0
         font = ImageFont.truetype(font=self._title_font, size=font_size)
-        text_draw.text(xy=(0, 0), text=self._title, fill=self._title_color, font=font)
+        for line in self._title.splitlines(keepends=False):
+            text_draw.text(xy=(x, y), text=self._title, fill=self._title_color, font=font)
+            y += font.getbbox(line)[1]
         resp = None
         while resp is None:
             try:
                 resp = ark_client.images.generate(
                     model=ark_image_model,
-                    prompt=json.dumps(self._prompt, ensure_ascii=False) + '\n注意: 请避免生成NSFW和地缘政治内容.',
+                    prompt=json.dumps(self._prompt, ensure_ascii=False) + '\n注意: 请避免生成NSFW和地缘政治内容, 请避免生成任何文字.',
                     size=f'{size[0]}x{size[1]}',
                     response_format='b64_json',
                     watermark=False,
-                    guidance_scale=5
+                    guidance_scale=4.5
                 ).data[0].b64_json
             except ArkBadRequestError as e:
                 log.error(e)
@@ -73,11 +79,11 @@ class CoverImitation(object):
 
 if __name__ == '__main__':
     from covgen.util.image_stringifier import stringify
-    image = CoverImitation(title='The Secret I Heard',
+    image = CoverImitation(title='The Secret\nI Heard',
                            base64_image=stringify(r'D:\project\covgen\resource\knowledge_base\female_oriented\modern_romance\western_realistic\img_3.png'),
                            image_format='png',
-                           title_color='white',
-                           title_font=r'D:\project\covgen\resource\font\Gabriola.ttf',
-                           title_height_correction=-300
+                           title_color='gold',
+                           title_font=r'D:\project\covgen\resource\font\HIGH SUMMIT.ttf',
+                           title_height_correction=-448
                            ).generate()
     image.save(r'D:\project\covgen\test\output\cover_imitation.png')
